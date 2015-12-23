0f47ff293c54e98d0e2209ecb8ada27a360fd80c
TJQKAS <A26052013@GMAIL.COM>
2015-12-23 23:16:48 +0000

    update jsapp

--- jsapp/engine.js
+++ jsapp/engine.js
@@ -23,7 +23,6 @@ var arruniq = arraname.filter(function (element, index, arraname) {
 });
 
 // this function create hash in the left - name in the right - frequency
-
 var often = arraname.reduce(function (acc, curr) {
   if (typeof acc[curr] == 'undefined') {
     acc[curr] = 1;
@@ -36,16 +35,37 @@ var often = arraname.reduce(function (acc, curr) {
 //by this way we show length of hash (often)
 var long = Object.keys(often).length
 
+//show length of each element in the array
+var arrauniq = arruniq.map(function(name) {
+  return name.length;
+});
 
- console.log(often);
- console.log(long);
- console.log(arruniq);
- console.log(arruniq.length);
- console.log(arraname);
- console.log(arraname.length);
+//we choose all elements with key flavor from data array
+var withflavor = data.filter(function (value) {
+    return (value.flavor);
+});
+
+//Frequency of elements with the same quantity of charts in the array arrauniq:
+var freq = arrauniq.reduce(function (acc, curr) {
+  if (typeof acc[curr] == 'undefined') {
+    acc[curr] = 1;
+  } else {
+    acc[curr] += 1;
+  }
+  return acc;
+}, {});
 
 
-
+ console.log("Our initial array",data);
+ console.log("Frequency of element's repeating in array:", often);
+ console.log("Length of unique array:",long);
+ console.log("Unique array of names:",arruniq);
+ console.log("Length of unique array:",arruniq.length);
+ console.log("Array of names:",arraname);
+ console.log("Length of array of names:",arraname.length);
+ console.log("Length of each name in unique array of names:",arrauniq);
+ console.log("All elements from data with flavor:", withflavor);
+ console.log("Frequency of elements with the same quantity of charts in the array arrauniq:", freq);
 }
 });
 
