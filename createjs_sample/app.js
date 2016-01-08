f7874274860b4c5f8413d6adab912da87b9b37c1
TJQKAS <A26052013@GMAIL.COM>
2016-01-08 16:20:48 +0000

    add some trash

--- createjs_sample/app.js
+++ createjs_sample/app.js
@@ -12,18 +12,24 @@ var queue;
  function handleComplete(event){
    var ball = new createjs.Shape();
    ball.addEventListener("click",handleClick);
-   ball.graphics.beginFill("#0004ff").drawCircle(0,0,80);
+   ball.graphics.beginFill("#0004ff").drawCircle(0,0,20);
    ball.x = 80;
    ball.y = 220;
-
    createjs.Tween.get(ball, {loop:true}).to({x:500}, 5000).to({x:50}, 5000);
    createjs.Ticker.addEventListener("tick", tick);
    stage.addChild(ball);
  }
  function handleClick(event) {
+   var ball = new createjs.Shape();
+   ball.graphics.beginFill("#0004ff").drawCircle(0,0,Math.random()*100);
+   createjs.Tween.get(ball, {loop:true}).to({x:Math.random()*1000,y:Math.random()*1000},750).to({x:Math.random()*1000,y:Math.random()*1000},750);
+   ball.x = Math.random()*1000;
+   ball.y = Math.random()*1000;
+   stage.addChild(ball);
+
    var bmp = new createjs.Bitmap(queue.getResult("alien"));
-   bmp.x = Math.random()*500;
-   bmp.y = Math.random()*500;
+   bmp.x = Math.random()*1000;
+   bmp.y = Math.random()*1000;
    stage.addChild(bmp);
    createjs.Sound.play("sound");
    console.log("hit to ball");
