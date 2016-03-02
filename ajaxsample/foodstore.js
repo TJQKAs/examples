f6ef9b41a95d2d4af4bdba4042a24ed74fabf88c
TJQKAS <A26052013@GMAIL.COM>
2016-03-02 22:20:01 +0000

    unsolved problems

--- ajaxsample/foodstore.js
+++ ajaxsample/foodstore.js
@@ -1,12 +1,13 @@
 // store object in var, which is responsible for communicating with php or xml file
+var firstLoad;
 var xmlHttp = createXmlHttpRequestObject();
 // 1- Basic fucntion!!!  get object and store to var  !!! it's important to use this function in all ajax script
 function createXmlHttpRequestObject(){
-    var xmlHttp;
+var xmlHttp;
 // by this conditonal statement we check whether user us IE as browser
     if(window.ActiveXObject){
       try{
-        xmlHttp = new ActiveXObject("Microsoft.XMLTTP");
+        xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
       } catch(e){
          xmlHttp = false;
       }
@@ -15,28 +16,50 @@ function createXmlHttpRequestObject(){
         xmlHttp = new XMLHttpRequest();
       } catch(e){
          xmlHttp = false;
-
     }
 }
-if (!xmlHttp){
+
+if (!xmlHttp)
 alert("can't create that object cause of some problem!");
-}
-else {
+
+else
   return xmlHttp;
+  console.log(xmlHttp);
 }
 // 2-Basic funciton!!!
-function process(){
+function firstLoad(){
   // firstly we check status of our object - 0 - ready for communicate;
   if(xmlHttp.readyState==0 || xmlHttp.readyState==4){
     // assign to var food data which has been typed in input form with id userInput
-    var food = encodeURIComponent(document.getElementById("userInput").value);
+    food = encodeURIComponent(document.getElementById("userInput").value);
     //open creates requset with 3 params -  first param -  what type of request we send (GET), second  param - url ; third param - true (should this request handle synchronously)
-   xmlHttp.open("GET","foodstore.php?food=" + food,  true);
+   xmlHttp.open("GET", "foodstore.php?food=" + food,  true);
   //  whenever we get back our info we want do smth with this info (whenever we get back response from server we want start function  3 Basic function - handleServerResponse)
-   xmlHttp.onreadystatechange = handleServerResponse;
+   xmlHttp.onreadystatechange = handleServerResponse();
    xmlHttp.send(null);
   }else{
-    // wait  1 sec and try to request again 
-   setTimeout('process()', 1000);
+    // wait  1 sec and try to request again
+   setTimeout(firstLoad, 1000);
+  }
+}
+
+
+function handleServerResponse(){
+  // object is ready to communticate if we have 4;
+  if(xmlHttp.readyState==4){
+    // communication successful if we have figure  - 200; else we have some problem with server or xml file;
+    if(xmlHttp.status==200){
+      // assign to var xmlResonse response of php file
+        xmlResponse = xmlHttp.responseXML;
+        xmlDocumentElement = xmlResponse.documentElement;
+        // by this way we access to our messages  in  php file  and data gives you data from xml file; this message is what we gonna put on our screen
+        message = xmlDocumentElement.firstChild.data;
+        // find form with id - underInput and put there our var message;
+        document.getElementById("underInput").innerHTML = "<span style='color:red'>"  + message + " </span>";
+        console.log(message);
+        setTimeout('firstLoad', 1000);
+    } else {
+      alert("Something went wrong!!!!");
+    }
   }
 }
--- ajaxsample/foodstore.php
+++ ajaxsample/foodstore.php
@@ -8,21 +8,17 @@ echo '<response>';
 // we store input data to var food
     $food = $_GET['food'];
     // and this is our database??? -  well, simple array with data
-    $foodArray = array('tuna', 'bacon', 'beef', 'pork', 'tea', 'fryed cheese', 'bread','ham' );
+    $foodArray = array('tuna', 'bacon', 'beef', 'pork', 'tea', 'fryed cheese', 'bread', 'ham' );
  // in_array is php function which checks whether input data is contained in our array data, and has two params 1) var - food 2) name of array - foodArray in which we checke it
-    if (in_array($food, $foodArray))
+    if (in_array($food,  $foodArray))
     // postive answer
       echo 'We do have  '.$food.' ! ';
-      elseif($food=="")
+      elseif ($food==' ')
       // in empty input case
       echo  'Please enter a food which you would like to order';
       else
       // negative answer
       echo "Sorry but we haven't".$food.'!';
 
-
 echo'</response>';
-
-
-
  ?>>
