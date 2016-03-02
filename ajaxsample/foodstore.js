c8fa56996d9c8852e7b01b53b6649bf097a247ac
TJQKAS <A26052013@GMAIL.COM>
2016-03-02 19:44:38 +0000

    ajaxsample-create function hSr

--- ajaxsample/foodstore.js
+++ ajaxsample/foodstore.js
@@ -1,6 +1,6 @@
 // store object in var, which is responsible for communicating with php or xml file
 var xmlHttp = createXmlHttpRequestObject();
-// get object and store to var  !!! it's important to use this function in all ajax script
+// 1- Basic fucntion!!!  get object and store to var  !!! it's important to use this function in all ajax script
 function createXmlHttpRequestObject(){
     var xmlHttp;
 // by this conditonal statement we check whether user us IE as browser
@@ -23,4 +23,20 @@ alert("can't create that object cause of some problem!");
 }
 else {
   return xmlHttp;
+}
+// 2-Basic funciton!!!
+function process(){
+  // firstly we check status of our object - 0 - ready for communicate;
+  if(xmlHttp.readyState==0 || xmlHttp.readyState==4){
+    // assign to var food data which has been typed in input form with id userInput
+    var food = encodeURIComponent(document.getElementById("userInput").value);
+    //open creates requset with 3 params -  first param -  what type of request we send (GET), second  param - url ; third param - true (should this request handle synchronously)
+   xmlHttp.open("GET","foodstore.php?food=" + food,  true);
+  //  whenever we get back our info we want do smth with this info (whenever we get back response from server we want start function  3 Basic function - handleServerResponse)
+   xmlHttp.onreadystatechange = handleServerResponse;
+   xmlHttp.send(null);
+  }else{
+    // wait  1 sec and try to request again 
+   setTimeout('process()', 1000);
+  }
 }
