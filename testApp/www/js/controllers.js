ffbaa78e64d7bcf4c692fffac198b1fd4d12663b
TJQKAS <A26052013@GMAIL.COM>
2016-03-17 19:28:18 +0000

    ionic 012 starter app code

--- testApp/www/js/controllers.js
+++ testApp/www/js/controllers.js
@@ -8,6 +8,14 @@ angular.module('starter.controllers', [])
   // listen for the $ionicView.enter event:
   //$scope.$on('$ionicView.enter', function(e) {
   //});
+  $scope.playlists = [
+    { title: 'test value', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
 
   // Form data for the login modal
   $scope.loginData = {};
@@ -39,18 +47,23 @@ angular.module('starter.controllers', [])
       $scope.closeLogin();
     }, 1000);
   };
+  // console.log("app ctrl");
 })
 
 .controller('PlaylistsCtrl', function($scope) {
   $scope.playlists = [
-    { title: 'Reggae', id: 1 },
+    { title: 'array object title property defined the second item', id: 1 },
     { title: 'Chill', id: 2 },
     { title: 'Dubstep', id: 3 },
     { title: 'Indie', id: 4 },
     { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
+    { title: 'Cowbell', id: 6 },
+      { title: 'new data from controller', id: 7 }
   ];
+  // console.log("playlists ctrl");
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
+  $scope.dynamicViewStateFromParam = $stateParams.id;
+// console.log("playlist ctrl");
 });
