18d4222c41bc668f3bdd772283fd6e52f27fb5f0
TJQKAS <A26052013@GMAIL.COM>
2016-03-17 17:25:46 +0000

    angular2 manipulating elems 1-1

--- angular-2-b/dev/my-component.component.ts
+++ angular-2-b/dev/my-component.component.ts
@@ -3,13 +3,14 @@ import{Component} from 'angular2/core';
 @Component({
   // here we make all configuration of our component
   selector: 'my-component',
+  // we put in span condition which use css class is-awesome  in case we answer 'yes'
   template: `
   <h1>this is my component!</h1>
-  Hi my name is {{name}}, this my first app ,<span [class.is-awesome]="inputElement.value=== 'yes' "> I think it's very nice</span>
+  Hi my name is {{name}}, this my first app ,<span [class.is-awesome]= "inputElement.value=== 'yes' "  [class.is-aweful]= "inputElement.value=== 'no' "    > I think it's very nice</span>
      <br/><br/>
      Am I rigth?
      <input type="text" #inputElement (keyup)="0">`,
-     styleUrls: ['src/css/component.css']
+     styleUrls: ['src/css/mycomponent.css']
 })
 // i'm going to export this class and use it outside
 export class MyComponentComponent {
