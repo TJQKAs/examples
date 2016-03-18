7b0e9c779387aca697ea05221915a6ef76c13295
TJQKAS <A26052013@GMAIL.COM>
2016-03-18 17:14:41 +0000

    angular2 tut manipulating templates

--- angular-2-b/dev/my-component.component.ts
+++ angular-2-b/dev/my-component.component.ts
@@ -4,12 +4,17 @@ import{Component} from 'angular2/core';
   // here we make all configuration of our component
   selector: 'my-component',
   // we put in span condition which use css class is-awesome  in case we answer 'yes'
+  // in template we can observe various directives - style directives with condition of name color
+  // directive for button disability  which depends on condition inputElement 
   template: `
   <h1>this is my component!</h1>
-  Hi my name is {{name}}, this my first app ,<span [class.is-awesome]= "inputElement.value=== 'yes' "  [class.is-aweful]= "inputElement.value=== 'no' "    > I think it's very nice</span>
+  Hi my name is <span [style.color]="inputElement.value==='yes' ? 'cyan' : 'green'">{{name}}</span>, this my first app ,<span [class.is-awesome]= "inputElement.value=== 'yes' "  [class.is-aweful]= "inputElement.value=== 'no' "    > I think it's very nice</span>
      <br/><br/>
      Am I rigth?
-     <input type="text" #inputElement (keyup)="0">`,
+     <input type="text" #inputElement (keyup)="0">
+     <br/>
+     <br/>
+     <button [disabled]="inputElement.value  === 'yes'">only if I type yes</button>`,
      styleUrls: ['src/css/mycomponent.css']
 })
 // i'm going to export this class and use it outside
