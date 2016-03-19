f247eab9c9ae12092b54c1391dc6f12622c56fdd
TJQKAS <A26052013@GMAIL.COM>
2016-03-19 14:17:46 +0000

    angular-2 logic of game

--- angular-2-b/dev/app.component.ts
+++ angular-2-b/dev/app.component.ts
@@ -3,32 +3,37 @@ import {Component} from 'angular2/core';
 @Component({
   // find tag in html with the name my-app and execute the code below in
     selector: 'my-app',
-    // by template we specify action here is just string, but we can set a link or extra document
-    //for exampleby templateUrl - http:// ....
-    //
+
+  //  we set class puzzle everywhere, and class solved depending on whether input values are equals to switchNumbers or not
     template: `
        <section class="setup">
        <h2>Game Setup</h2>
                Enter your name dude:
-             <input type="text">
+             <input type="text" #name (keyup)="0">
        </section>
-       <section>
+       <section
+       [ngClass]="{
+         puzzle: true,
+         solved: switch1.value == switchNumber1 && switch2.value ==
+         switchNumber2 && switch3.value == switchNumber3 && switch4.value ==
+         switchNumber4
+       }"
+
+
+       [style.display]="name.value === '' ? 'none' : 'block' ">
          <h2>The Puzzle!</h2>
          <p>Welcome to the game<span class="name">XXX</span></p>
          <br/>
          Switch 1:
-         <input type="text"><br/>
+         <input type="text" #switch1><br/>
          Switch 2:
-         <input type="text"><br/>
+         <input type="text" #switch2><br/>
          Switch 3:
-         <input type="text"><br/>
+         <input type="text" #switch3><br/>
          Switch 4:
-         <input type="text"><br/>
+         <input type="text" #switch4><br/>
        </section>
        <h2>Congrats XXX!  you did it</h2>
-
-
-
 
     `,
 
@@ -36,5 +41,8 @@ import {Component} from 'angular2/core';
 })
 // this our default class
 export class AppComponent {
-
+ switchNumber1: number;
+ switchNumber2: number;
+ switchNumber3: number;
+ switchNumber4: number;
 }
