4844fbf425e97b697dcb79bc1eab08232865b500
TJQKAS <A26052013@GMAIL.COM>
2016-03-19 13:39:31 +0000

    angular-2 shape of the game's elems

--- angular-2-b/dev/app.component.ts
+++ angular-2-b/dev/app.component.ts
@@ -6,7 +6,31 @@ import {Component} from 'angular2/core';
     // by template we specify action here is just string, but we can set a link or extra document
     //for exampleby templateUrl - http:// ....
     //
-    template: `Random text `,
+    template: `
+       <section class="setup">
+       <h2>Game Setup</h2>
+               Enter your name dude:
+             <input type="text">
+       </section>
+       <section>
+         <h2>The Puzzle!</h2>
+         <p>Welcome to the game<span class="name">XXX</span></p>
+         <br/>
+         Switch 1:
+         <input type="text"><br/>
+         Switch 2:
+         <input type="text"><br/>
+         Switch 3:
+         <input type="text"><br/>
+         Switch 4:
+         <input type="text"><br/>
+       </section>
+       <h2>Congrats XXX!  you did it</h2>
+
+
+
+
+    `,
 
 
 })
