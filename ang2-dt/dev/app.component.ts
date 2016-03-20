fa594853897fee4d211f28ef02c02b840497a8ab
TJQKAS <A26052013@GMAIL.COM>
2016-03-20 18:02:51 +0000

    ang-2 two-way-databinding

--- ang2-dt/dev/app.component.ts
+++ ang2-dt/dev/app.component.ts
@@ -8,10 +8,15 @@ import {Component} from 'angular2/core';
   // [value]="name" - prop binding first case; everything with prefix ng - is directive [ngClass]="{hyper: true};
   //(keyup)="onKeyup() - whenever we listen events and catch them by (keyup)  we want fire func onKeyup();
   // set the var inputElem and put value of this var  in func , so we can make oprations with him
+
+  //   <input type="text" [(ngModel)]="name"> two way data binding - which handle input and output data from the same var
  template: `<h1 class="{{'hyper'}}">{{onTest()}}</h1>
  <br/>
      <input type="text" [value]="name"  [ngClass]="{hyper: true}"  (keyup)="onKeyup(inputElem.value)" #inputElem>
     <p>{{values}}<br/></p>
+    <br/><br/><br/>
+    <input type="text" [(ngModel)]="name">
+    <p>Your name : {{name}}</p>
   `
 
 })
