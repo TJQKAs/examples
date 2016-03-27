6a5a5879ec96365b1f727065d7777767c32e6df0
TJQKAS <A26052013@GMAIL.COM>
2016-03-27 16:30:16 +0100

    Ang2 structural directive ngFor

--- angular-2-b/dev/structural-directive.component.ts
+++ angular-2-b/dev/structural-directive.component.ts
@@ -11,11 +11,22 @@ import {Component} from 'angular2/core';
       <input type="text" #number (keyup)="0">
       </div>
     <div *ngIf="number.value > 0 && number.value < 51">
-    <h4>All elements which stand between tags with *ngIf <br/>
+    <h4 class="underlined">All elements which stand between tags with *ngIf <br/>
     are saw if condition is true </h4>
     Number belongs to the interspace </div>
-  </section>`,
+  </section>
+  <section class="directive">
+    <h2>*ngFor</h2>
+    <div>
+      <ul>
+        <li class="underlined" *ngFor="#item of list, #i = index">{{item}} - number {{i+1}}</li>
+      </ul>
+    </div>
+  </section>`
+
+
+  ,
 })
 export class StructuralDirectives{
 
-}
+    list =  ['Acai','Apple','Apricots','Avocado ','Banana','Blackberry','Blueberries','Cherries','Currents','Cucumber','Goji berries','Durian'] ;
