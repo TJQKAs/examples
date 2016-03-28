2a1f638a5e5173773ac9a8949e4f503ee5264625
TJQKAS <A26052013@GMAIL.COM>
2016-03-28 16:27:01 +0100

    testApp style of the news card

--- testApp/www/js/filters.js
+++ testApp/www/js/filters.js
@@ -1,4 +1,33 @@
 angular.module('testApp.filters', [])
+
+.filter('characters', function(){
+  return function(input, chars, breakOnWord, ellipsis){
+    if(isNaN(chars)) return input;
+    if(chars <= 0) return '';
+    if(input && input.length > chars){
+      input = input.substring(0, chars);
+
+      if(!breakOnWord){
+        var lastspace = input.lastIndexOf(' ');
+        if(!lastspace !== -1){
+          input = input.substr(0, lastspace);
+        }
+      }
+      else {
+        while(input.charAt(input.length -1)=== ' '){
+          input = input.substr(0, input.length -1);
+        }
+      }
+      if(ellipsis) return input + '-';
+      if(!ellipsis) return input; 
+    }
+    return input;
+  };
+})
+
+
+
+
 // filter modifies figures reducing zerooos
 .filter("shrinkNumber", function($filter){
   return  function(number, fractionSize){
