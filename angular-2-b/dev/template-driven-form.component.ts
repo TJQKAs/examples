9ee9edf0eef8f14b89b3dfa21a511a1e1b91b9a1
TJQKAS <A26052013@GMAIL.COM>
2016-04-06 22:42:06 +0100

    Ang-2 template-dr-app: add condion if all imput form typed correctly div-elem appears with typed data

--- angular-2-b/dev/template-driven-form.component.ts
+++ angular-2-b/dev/template-driven-form.component.ts
@@ -11,15 +11,18 @@ import{Component}from 'angular2/core';
    <form (ngSubmit)="onSubmit(f)" #f="ngForm">
      <div>
        <label for="name">Name:</label>
-       <input ngControl="name" type="text" id="name" required>
+       <input ngControl="name" type="text" id="name" #name="ngForm" required>
+      <span class="validaton-error" *ngIf="!name.valid">not valid</span>
      </div>
      <div>
        <label for="surname">Surname:</label>
-       <input ngControl="surname" type="text" id="surname" required>
+       <input ngControl="surname" type="text" id="surname" #surname="ngForm" required>
+       <span class="validaton-error" *ngIf="!surname.valid">not valid</span>
      </div>
      <div>
        <label for="age">Age:</label>
-       <input ngControl="age" type="number" id="age" required>
+       <input ngControl="age" type="number" id="age" #age="ngForm" required>
+        <span class="validaton-error" *ngIf="!age.valid">not valid</span>
      </div>
      <div>
        <label for="country">Country:</label>
@@ -27,24 +30,41 @@ import{Component}from 'angular2/core';
      </div>
      <div>
        <label for="mail">Mail:</label>
-       <input ngControl="mail" type="text" id="mail" required>
+       <input ngControl="mail" type="text" id="mail" #mail="ngForm" required>
+       <span class="validaton-error" *ngIf="!mail.valid">not valid</span>
      </div>
      <div>
        <label for="password">Password:</label>
-       <input ngControl="password" type="text" id="name" required>
+       <input ngControl="password" type="text" id="name" #password="ngForm" required>
+        <span class="validaton-error" *ngIf="!password.valid">not valid</span>
      </div>
      <div>
        <label for="confirm-password">Confirm Password:</label>
-       <input ngControl="confirm-password" type="text" id="confirm-password" required>
+       <input ngControl="confirm-password" type="text" id="confirm-password" #confirmPassword="ngForm" required>
+        <span class="validaton-error" *ngIf="!confirmPassword.valid">not valid</span>
      </div>
-     <button type="submit">Submit</button>
-   </form>
+     <button type="submit" [disabled]="!f.valid || password.value !== confirmPassword.value">Submit</button>
+      </form>
+   <div [style.display]="f.valid === false ? 'none' : 'block' ">
+     <h4>Data which you've just typed</h4>
+     <h5>name: {{user.name}}</h5>
+     <h5>surname: {{user.surname}}</h5>
+     <h5>age: {{user.age}}</h5>
+     <h5>mail: {{user.mail}}</h5>
+     <h5>counrty: {{user.age}}</h5>
+   </div>
+
   `
 })
 
 export class TemplateDrivenFromComponent{
+ user ={name: ' ', surname: ' ', age: ' ', mail: ' ', country: ' '};
 
-  onSubmit(value){
-    console.log(value.value);
+  onSubmit(form){
+     this.user.name = form.value['name'];
+        this.user.surname = form.value['surname'];
+           this.user.age = form.value['age'];
+              this.user.mail = form.value['mail'];
+                 this.user.country = form.value['country'];
   }
 }
